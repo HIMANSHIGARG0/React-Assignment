8f8dc181826f340ef950cae59edd505a703091ac
HIMANSHIGARG0 <59911323+HIMANSHIGARG0@users.noreply.github.com>
2021-09-07 16:25:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Body from './Components/Body';
+
+
+function App() {
+  return (
+   <>
+   <div className = "doc">
+      <Header></Header>
+      <Body></Body>
+   </div>
+     
+   </>
+  );
+}
+export default App;
